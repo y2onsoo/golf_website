743bca42af3e8af7d1bd3b2ea5f39a182acc778c
y2onsoo <abbcaog000@naver.com>
2023-05-16 07:59:21 +0900

    미디어쿼리 수정

---
+++ js/script.js
@@ -0,0 +1,133 @@
+//////////////날씨 API
+var weatherIcon = {
+  "01": "fas fa-sun",
+  "02": "fas fa-cloud-sun",
+  "03": "fas fa-cloud",
+  "04": "fas fa-cloud-meatball",
+  "09": "fas fa-cloud-sun-rain",
+  10: "fas fa-cloud-showers-heavy",
+  11: "fas fa-poo-storm",
+  13: "far fa-snowflake",
+  50: "fas fa-smog",
+};
+
+// 날씨 api - 강원도
+var apiURI =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  "gangwon-do" +
+  "&appid=" +
+  "cdad16695eef24059c08fb46ef92409f";
+$.ajax({
+  url: apiURI,
+  dataType: "json",
+  type: "GET",
+  async: "false",
+  success: function (resp) {
+    var $Icon = resp.weather[0].icon.substr(0, 2);
+    var $wind = "바람&nbsp;&nbsp;&nbsp;&nbsp;" + resp.wind.speed + " m/s";
+    var $city = "강원도";
+    var $temp_min =
+      "최저 온도&nbsp;&nbsp;&nbsp;&nbsp;" +
+      Math.floor(resp.main.temp_min - 273.15) +
+      "º";
+    var $temp_max =
+      "최고 온도&nbsp;&nbsp;&nbsp;&nbsp;" +
+      Math.floor(resp.main.temp_max - 273.15) +
+      "º";
+
+    $(".weather_icon").append(
+      '<i class="' +
+        weatherIcon[$Icon] +
+        ' fa-5x" style="height :30px; width :100px;"></i>'
+    );
+    $(".wind").prepend($wind);
+    $(".city").append($city);
+    $(".temp_min").append($temp_min);
+    $(".temp_max").append($temp_max);
+  },
+});
+
+/////////////서브메뉴 스크롤
+function scrollFunction() {
+  var headerScroll =
+    document.documentElement.scrollTop || document.body.scrollTop;
+  if (headerScroll > 300) {
+    $("#header").removeClass("head");
+    $("#header").addClass("scroll-header");
+  } else {
+    $("#header").removeClass("scroll-header");
+    $("#header").addClass("head");
+  }
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+//로그인폼
+window.onload = function () {
+  const pw_show_hide = document.querySelector(".pw_show_hide");
+  const input_id = document.querySelector("input[type=text]");
+  const input_pw = document.querySelector("input[type=password]");
+  const id_error = document.querySelector(".id_error");
+  const pw_error = document.querySelector(".pw_error");
+  // console.log(pw_show_hide, input_id, input_pw, id_error, pw_error);
+
+  input_id.addEventListener("click", function () {
+    id_error.style.display = "block";
+  });
+  input_pw.addEventListener("click", function () {
+    pw_error.style.display = "block";
+  });
+
+  let i = true;
+  pw_show_hide.addEventListener("click", function () {
+    if (i == true) {
+      pw_show_hide.style.backgroundPosition = "-126px 0";
+      i = false;
+    } else {
+      pw_show_hide.style.backgroundPosition = "-105px 0";
+      i = true;
+    }
+  });
+
+  ////////////////////////////widget 모달 모음
+  $(".login").click(function () {
+    $(".modal-login,.modal-login-bg").show();
+  });
+  $(".close-btn").click(function () {
+    $(".modal-login,.modal-login-bg").hide();
+  });
+  //
+  $(".course span").click(function () {
+    $(".mo-widget-wrap,.mo-widget-course").show();
+  });
+  $(".mo-course-close").click(function () {
+    $(".mo-widget-wrap,.mo-widget-course").hide();
+  });
+  //
+  $(".ham-bar").click(function () {
+    $("#gnb.mo-gnb").show();
+  });
+  $("#gnb.mo-gnb").click(function () {
+    $("#gnb.mo-gnb").hide();
+  });
+};
+
+$(document).ready(function () {
+  for (let i = 1; i < 10; i++) {
+    const id = `<span class="mo-course-span-${i}" onclick="showimg(${i})">${i}</span>`;
+    $("#holeNum").append(id);
+  }
+});
+
+//버튼 클릳시 이미지 보이게
+const showimg = (gubun) => {
+  $("#imgbox").empty();
+
+  $("#imgbox").append(
+    `
+    <div class="hole-img hole-img-${gubun}"></div>
+    `
+  );
+};
---
+++ js/sub.js
@@ -0,0 +1,90 @@
+//////////////날씨 API
+var weatherIcon = {
+  "01": "fas fa-sun",
+  "02": "fas fa-cloud-sun",
+  "03": "fas fa-cloud",
+  "04": "fas fa-cloud-meatball",
+  "09": "fas fa-cloud-sun-rain",
+  10: "fas fa-cloud-showers-heavy",
+  11: "fas fa-poo-storm",
+  13: "far fa-snowflake",
+  50: "fas fa-smog",
+};
+
+// 날씨 api - 강원도
+var apiURI =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  "gangwon-do" +
+  "&appid=" +
+  "cdad16695eef24059c08fb46ef92409f";
+$.ajax({
+  url: apiURI,
+  dataType: "json",
+  type: "GET",
+  async: "false",
+  success: function (resp) {
+    var $Icon = resp.weather[0].icon.substr(0, 2);
+    var $wind = "바람&nbsp;&nbsp;&nbsp;&nbsp;" + resp.wind.speed + " m/s";
+    var $city = "강원도";
+    var $temp_min =
+      "최저 온도&nbsp;&nbsp;&nbsp;&nbsp;" +
+      Math.floor(resp.main.temp_min - 273.15) +
+      "º";
+    var $temp_max =
+      "최고 온도&nbsp;&nbsp;&nbsp;&nbsp;" +
+      Math.floor(resp.main.temp_max - 273.15) +
+      "º";
+
+    $(".weather_icon").append(
+      '<i class="' +
+        weatherIcon[$Icon] +
+        ' fa-5x" style="height :30px; width :100px;"></i>'
+    );
+    $(".wind").prepend($wind);
+    $(".city").append($city);
+    $(".temp_min").append($temp_min);
+    $(".temp_max").append($temp_max);
+  },
+});
+
+// ///////////////////sub페이지-슬라이더
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+AOS.init();
+
+
+/////////topbutton
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
